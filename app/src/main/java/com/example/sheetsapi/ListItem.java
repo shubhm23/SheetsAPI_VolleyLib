8f3cc6f8635194e270466c7a26c25a8a846095e8
shubhm23 <1ms19cs118@gmail.com>
2022-02-11 20:43:35 +0530

    Implement GET Request with Activity

--- app/src/main/java/com/example/sheetsapi/ListItem.java
+++ app/src/main/java/com/example/sheetsapi/ListItem.java
@@ -1,4 +1,90 @@
 package com.example.sheetsapi;
 
-public class ListItem {
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ListItem extends AppCompatActivity {
+    ListView listView;
+    ListAdapter adapter;
+    ProgressDialog loading;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listitem);
+        listView = findViewById(R.id.lvItems);
+        showItems();
+    }
+
+    private void showItems() {
+        loading =  ProgressDialog.show(this,"Loading","please wait",false,true);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, BuildConfig.Apps_ScriptKey + "action=getItems",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        parseItems(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        );
+
+        int socketTimeOut = 50000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(stringRequest);
+    }
+
+    private void parseItems(String jsonResponse) {
+        ArrayList<HashMap<String, String>> list = new ArrayList<>();
+        try {
+            JSONObject jObj = new JSONObject(jsonResponse);
+            JSONArray jArray = jObj.getJSONArray("items");
+            for (int i = 0; i < jArray.length(); i++) {
+                JSONObject jo = jArray.getJSONObject(i);
+                String Name = jo.getString("Name");
+                String Hobby = jo.getString("Hobby");
+                HashMap<String, String> item = new HashMap<>();
+                item.put("Name", Name);
+                item.put("Hobby", Hobby);
+                list.add(item);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        adapter = new SimpleAdapter(this,list,R.layout.listitem_row,
+                new String[]{"Name", "Hobby"},new int[]{R.id.tv_Name,R.id.tv_Hobby});
+
+        listView.setAdapter(adapter);
+        loading.dismiss();
+    }
+
+
 }
